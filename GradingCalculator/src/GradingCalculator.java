d53581c98d1c1d69cd54fa8624df8aeeffd0f5af
tom0000009 <tom.h07@gmail.com>
2022-06-29 15:56:02 -0400

    GradingCalculator

---
+++ GradingCalculator/src/GradingCalculator.java
@@ -0,0 +1,32 @@
+
+public class GradingCalculator {
+
+	public static void main(String[] args) {
+
+		int [] arr = {95, 85, 62, 40, 79};
+
+		int [] grades= arr; 
+		String[]topic = {"Math", "English", "Science", "History", "Art"};
+		char[]letter=new char[grades.length];
+		int a=90, b=80, c=70, d=60;
+		
+		//fill letter array based on grade array
+		for(int i=0;i<grades.length;i++) {
+			if(grades[i]>a)
+				letter[i]='A';
+			else if(grades[i]>b)
+				letter[i]='B';
+			else if(grades[i]>c)
+				letter[i]='C';
+			else if(grades[i]>d)
+				letter[i]='D';
+			else
+				letter[i]='F';
+		}	
+		
+		System.out.println("Grades\n"+topic[0]+":"+letter[0]);
+		for(int i=1;i<grades.length;i++)
+			System.out.println(topic[0]+":"+letter[i]);
+	}
+
+}
