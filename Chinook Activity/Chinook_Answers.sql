7d34e168f52924177a19a83f5ef695bfb86f6d9e
Tommy Hai <tom.h07@gmail.com>
2022-07-15 14:42:10 -0400

    finished question 10

--- Chinook Activity/Chinook_Answers.sql
+++ Chinook Activity/Chinook_Answers.sql
@@ -1,6 +1,7 @@
 /*
 *1) Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
 *2) Provide a query only showing the Customers from Brazil.
+*10) Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
 */
 
 --Question 1
@@ -8,10 +9,27 @@
 --must be in "" because jdbc seems to set everything to lowercase to achieve case insensitivity
 
 select * from "Customer";
-
 select "FirstName" ,"LastName" ,"CustomerId" ,"Country"  from "Customer" ;
 
+--answer
 select "FirstName" ,"LastName" ,"CustomerId" ,"Country"  from "Customer" where "Country"!='USA';
+
 
 --Question 2
 select * from "Customer" where "Country"='Brazil';
+
+
+--Question 10
+--Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY
+
+select * from batches;
+SELECT "trainer_id", count(associate_id) FROM batches GROUP BY trainer_id 
+HAVING trainer_id > 1;
+
+select * from "InvoiceLine";
+select "InvoiceId" , count("InvoiceLineId") from "InvoiceLine" group by "InvoiceId" having "InvoiceId"=2;
+
+--answer
+select "InvoiceId" , count("InvoiceLineId") from "InvoiceLine" group by "InvoiceId" order by "InvoiceId";
+
+
